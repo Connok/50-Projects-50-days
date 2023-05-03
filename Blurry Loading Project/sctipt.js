7d145be35fdde4cf70dd13ffb7a5bb14b8c4a96a
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-05-02 20:38:14 -0400

    html and css done

--- Blurry Loading Project/sctipt.js
+++ Blurry Loading Project/sctipt.js
@@ -6,14 +6,13 @@ let int = setInterval(bluring, 30)
 
 function bluring() {
     load++
-    if(load > 99)
-    {
+    if (load > 99) {
         clearInterval(int)
     }
     loadText.innerText = `${load}%`
-    loadText.getElementsByClassName.opacity = scale(load, 0, 100, 1,0)
+    loadText.getElementsByClassName.opacity = scale(load, 0, 100, 1, 0)
     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
-const scale =(num, in_min, in_max,out_min, out_max) => {
+const scale = (num, in_min, in_max, out_min, out_max) => {
     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
