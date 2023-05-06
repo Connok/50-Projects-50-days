332ab1d2363ce74dd9799c38cab3fe65187ce95c
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-05-06 08:45:38 -0400

    PRoject 13 Finished

--- Random Choice PIcker/sctipt.js
+++ Random Choice PIcker/sctipt.js
@@ -4,13 +4,19 @@ const textArea = document.getElementById('textarea')
 textArea.focus()
 
 textArea.addEventListener('keyup', (e) => {
-    creatTags(e.target.value)
+  creatTags(e.target.value)
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = ''
+    }, 10)
+    randomSelect()
+  }
 })
 
-function creatTags(input){
- console.log(input)
- const tags = input.split(',').filter(tag => tag.trim()
-  !== '').map(tag => tag.trim())
+function creatTags(input) {
+  console.log(input)
+  const tags = input.split(',').filter(tag => tag.trim()
+    !== '').map(tag => tag.trim())
   tagsEl.innerHTML = " "
   tags.forEach(tag => {
     const tagEl = document.createElement('span')
@@ -18,4 +24,33 @@ function creatTags(input){
     tagEl.innerText = tag
     tagsEl.appendChild(tagEl)
   });
+}
+
+function randomSelect() {
+  const times = 30
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag()
+    highlightTag(randomTag)
+    setTimeout(() => {
+      unhighlightTag(randomTag)
+    }, 100)
+  }, 100)
+  setTimeout(() => {
+    clearInterval(interval)
+    setTimeout(() => {
+      const randomTag = pickRandomTag()
+      highlightTag(randomTag)
+    }, 100)
+  }, times * 100)
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+function highlightTag(tag) {
+  tag.classList.add('highlight')
+}
+function unhighlightTag(tag) {
+  tag.classList.remove('highlight')
 }
