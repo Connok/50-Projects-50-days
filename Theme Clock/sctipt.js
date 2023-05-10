dfaad4d787151deee11b62e9868678874b418049
Connor McCarthy <connokxbox@gmail.com>
2023-05-10 12:39:15 -0400

    Dark Mode Function added

--- Theme Clock/sctipt.js
+++ Theme Clock/sctipt.js
@@ -1,1 +1,20 @@
-// https://www.traversymedia.com/products/50-projects-in-50-days-html-css-javascript/categories/2152088045/posts/2164608631
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggle = document.querySelector('.toggle')
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if(html.classList.contains('dark')){
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark Mode'
+    }else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light Mode'
+    }
+})
