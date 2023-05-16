2f28e11d7cfa0f388b147e5902f8de30cd529e7f
Connor McCarthy <connokxbox@gmail.com>
2023-05-16 12:47:54 -0400

    drawing works

--- drawing_app/sctipt.js
+++ drawing_app/sctipt.js
@@ -3,9 +3,34 @@ const ctx = canvas.getContext('2d')
 
 
 let size = 20
+let isPressed = false
 let color = 'black'
 let x
 let y
+
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true
+    x = e.offsetX
+    y = e.offsetY
+})
+canvas.addEventListener('mouseup', (e) => {
+    isPressed = false
+    x = undefined
+    y = undefined
+    
+    
+})
+canvas.addEventListener('mousemove', (e) => {
+   if(isPressed = true){
+    const x2 = e.offsetX
+    const y2 = e.offsetY
+
+    drawCircle(x2,y2)
+    drawLine(x,y,x2,y2)
+    y=y2
+    x=x2
+   }
+})
 
 
 function drawCircle(x, y) {
@@ -20,7 +45,7 @@ function drawLine(x1, y1, x2, y2) {
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
     ctx.strokeStyle = color
-    ctx.lineWidth = size
+    ctx.lineWidth = size * 2
     ctx.stroke()
 }
 
