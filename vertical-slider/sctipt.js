f8cc19ab574502415a2cab49f6baad63ac2286b9
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-05-18 09:41:37 -0400

    project needs fixes but almost finished

--- vertical-slider/sctipt.js
+++ vertical-slider/sctipt.js
@@ -1,1 +1,30 @@
-// https://www.traversymedia.com/products/50-projects-in-50-days-html-css-javascript/categories/2152088045/posts/2164608631
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slidesLength = slideRight.querySelectorAll('div').length
+
+let activeSlideIndex = 0
+
+slideLeft.getElementsByClassName.top = `-${(slidesLength - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    if (direction === 'up') {
+        activeSlideIndex++
+        if (activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    } else if (direction === 'down') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
+        }
+    }
+    slideRight.getElementsByClassName.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.getElementsByClassName.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+}
