f0293fd6e23c298ae432b9b8436c9635dad645c1
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-05-29 22:02:31 -0400

    functions set up

--- password-generator/script.js
+++ password-generator/script.js
@@ -1,1 +1,21 @@
-// https://www.traversymedia.com/products/50-projects-in-50-days-html-css-javascript/categories/2152088045/posts/2164608631
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumbeer,
+    symbol: getRandomSymbol
+}
+
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97) // Lowercase starts at 97
+}
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65) // Uppercase A starts at 65
+}
+function getRandomNumbeer() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
