97cad9e8fba688376d110d23c88ec8cd112d97cf
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-06-28 20:49:23 -0400

    hoberboard finished

--- hover-board/script.js
+++ hover-board/script.js
@@ -1,1 +1,32 @@
-// https://www.traversymedia.com/products/50-projects-in-50-days-html-css-javascript/categories/2152088045/posts/2164608631
+const container = document.getElementById('container')
+
+const colors = ['#e74c3c', 'red', 'blue', 'green', 'violet', 'white',]
+
+const SQUARES = 800
+
+for (let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseout', () => removeColor(square))
+    
+
+    container.appendChild(square)
+}
+ 
+function setColor(element) { 
+    const color = getRandomColor()
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color} `
+}
+
+function removeColor(element) { 
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
+    console.log('hi')
+}
+
+function getRandomColor() { 
+    return colors[Math.floor(Math.random() * colors.length)]
+}
